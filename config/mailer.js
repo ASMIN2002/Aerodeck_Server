45289da0de9cbf82d86fc0394adda57c4622f297
Asmin Kuldeep Jena <asminkuldeep6@gmail.com>
2026-06-30 14:45:16 +0530

    Added Founder Management and Cloudinary Upload

--- config/mailer.js
+++ config/mailer.js
@@ -2,15 +2,31 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
 
-  service: "gmail",
+    service: "gmail",
 
-  auth: {
+    auth: {
 
-    user: process.env.MAIL_EMAIL,
+        user: process.env.MAIL_EMAIL,
 
-    pass: process.env.MAIL_PASSWORD
+        pass: process.env.MAIL_PASSWORD
 
-  }
+    }
+
+});
+
+transporter.verify((error, success) => {
+
+    if (error) {
+
+        console.log("SMTP VERIFY ERROR:");
+
+        console.log(error);
+
+    } else {
+
+        console.log("SMTP READY");
+
+    }
 
 });
 
